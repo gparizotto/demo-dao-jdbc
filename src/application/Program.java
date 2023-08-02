37e8bbea6947d8d9a7d1d646dbbb79ae80c71152
gparizotto <g.parizotto@unesp.br>
2023-08-02 14:51:28 -0300

    Initial Commit

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+		SellerDao sellerdao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerdao.findById(3);
+		
+		System.out.println(seller);
+
+	}
+
+}
